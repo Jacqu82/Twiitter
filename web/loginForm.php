a0dd7b771b6b7380c2f83656096fd15ddf23531b
Jacqu82 <jacqu25@yahoo.com>
2017-05-21 20:31:00 +0200

    bootstrap added

--- web/loginForm.php
+++ web/loginForm.php
@@ -1,27 +1,57 @@
 <!DOCTYPE html>
-
-<html>
+<html lang="pl">
     <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Login</title>
+        <link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
+        <link href="../css/style.css" rel="stylesheet">
     </head>
     <body>
-        <form method="POST" action="login.php">
-            <p>Zaloguj się na swoje konto</p>
-            <p>
-                <label>
-                    Login: <input name="username" type="text">
-                </label>
-            </p>
-            <p>
-                <label>
-                    Hasło: <input name="password" type="password">
-                </label>
-            </p>
-            <p>
-                <input type="submit" value="Zaloguj się">
-            </p>          
+    <div class="container">
+        <h3>Zaloguj się na swoje konto</h3>
+        <form method="POST" action="login.php" class="form-horizontal">
+            <div class="form-group">
+                <label for="nameField" class="col-xs-2">Login:</label>
+                <div class="col-xs-3">
+                    <input type="text" name="username" class="form-control" id="nameField" placeholder="Login" />
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="passwordField" class="col-xs-2">Hasło:</label>
+                <div class="col-xs-3">
+                    <input type="password" name="password" class="form-control" id="passwordField" placeholder="Hasło" />
+                </div>
+            </div>
+            <div class="col-xs-4">
+                <button type="submit" class="btn btn-primary">Zaloguj się</button>
+            </div>
         </form>
-            <p>Nie masz konta? <a href="registerForm.php">Zarejestruj się</a></p>
+    </div>
+    <hr/>
+    <div class="container">
+        Nie masz konta? <a href="registerForm.php">Zarejestruj się</a>
+    </div>
+<!--        <form method="POST" action="login.php">-->
+<!--            <p>Zaloguj się na swoje konto</p>-->
+<!--            <p>-->
+<!--                <label>-->
+<!--                    Login: <input name="username" type="text">-->
+<!--                </label>-->
+<!--            </p>-->
+<!--            <p>-->
+<!--                <label>-->
+<!--                    Hasło: <input name="password" type="password">-->
+<!--                </label>-->
+<!--            </p>-->
+<!--            <p>-->
+<!--                <input type="submit" value="Zaloguj się">-->
+<!--            </p>-->
+<!--        </form>-->
+
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js"
+            integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+            crossorigin="anonymous"></script>
+    <script src="../js/bootstrap.js"></script>
     </body>
 </html>
--- web/registerForm.php
+++ web/registerForm.php
@@ -1,17 +1,52 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Register</title>
-    </head>
-    <body>
-        <p>Masz konto? <a href="loginForm.php">Zaloguj się</a></p>
-        <p>Utwórz nowe konto na Twiterze</p>
-        <form method="POST" action="register.php">
-            <p><label>Login: </label><input name="username" type="text"></label></p>
-            <p><label>Email: <input name="email" type="email"></label></p>
-            <p><label>Hasło: <input name="password" type="password"></label></p>
-            <p><label><input type="submit" value="Zarejestruj się"></label></p>
-        </form>
-    </body>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Register</title>
+    <link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
+    <link href="../css/style.css" rel="stylesheet">
+</head>
+<body>
+<div class="container">
+    <h3>Masz konto? <a href="loginForm.php">Zaloguj się</a></h3>
+    <h3>Utwórz nowe konto na Twiterze</h3>
+    <form method="POST" action="register.php" class="form-horizontal">
+        <div class="form-group">
+            <label for="nameField" class="col-xs-2">Login:</label>
+            <div class="col-xs-3">
+                <input type="text" name="username" class="form-control" id="nameField" placeholder="Login"/>
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="emailField" class="col-xs-2">E-mail</label>
+            <div class="col-xs-3">
+                <input type="email" name="email" class="form-control" id="emailField" placeholder="E-mail"/>
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="passwordField" class="col-xs-2">Hasło:</label>
+            <div class="col-xs-3">
+                <input type="password" name="password" class="form-control" id="passwordField" placeholder="Hasło"/>
+            </div>
+        </div>
+        <div class="col-xs-4">
+            <button type="submit" class="btn btn-primary">Zarejestruj się</button>
+        </div>
+    </form>
+</div>
+
+<!--        <p>Masz konto? <a href="loginForm.php">Zaloguj się</a></p>-->
+<!--        <p>Utwórz nowe konto na Twiterze</p>-->
+<!--        <form method="POST" action="register.php">-->
+<!--            <p><label>Login: </label><input name="username" type="text"></label></p>-->
+<!--            <p><label>Email: <input name="email" type="email"></label></p>-->
+<!--            <p><label>Hasło: <input name="password" type="password"></label></p>-->
+<!--            <p><label><input type="submit" value="Zarejestruj się"></label></p>-->
+<!--        </form>-->
+<script src="https://code.jquery.com/jquery-3.1.1.min.js"
+        integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+        crossorigin="anonymous"></script>
+<script src="../js/bootstrap.js"></script>
+</body>
 </html>
